ed2fc12a27772f8b8885caf9168e2887a101be83
Chithra582 <chithu582@gmail.com>
2026-01-21 22:17:01 +0530

    Fixes the Bug in Backend folder

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -5,31 +5,43 @@ const EventSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
   locationCenter: {
     type: {
       type: String,
-      enum: ['Point'],
-      required: true
+      enum: ["Point"],
+      required: true,
     },
     coordinates: {
       type: [Number],
       required: true,
-    }
+      validate: {
+        validator: function (value) {
+          return value.length === 2;
+        },
+        message: "Coordinates must be in [longitude, latitude] format",
+      },
+    },
   },
+
   radius: {
     type: Number,
     required: true,
   },
+
   photoCount: {
     type: Number,
+    default: 0,
+    required: true,
   },
+
   createdAt: {
     type: Date,
     default: Date.now,
-  }
+  },
 });
 
-EventSchema.index({ locationCenter: '2dsphere' });
+EventSchema.index({ locationCenter: "2dsphere" });
 
-const Event = mongoose.model('Event', EventSchema);
-export default Event
+const Event = mongoose.model("Event", EventSchema);
+export default Event;
